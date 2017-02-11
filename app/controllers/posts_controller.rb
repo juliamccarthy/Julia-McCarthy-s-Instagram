1b8fd291d90afc5dcf6155f16b12738cd8761511
Julia McCarthy <jmccarthy18@students.claremontmckenna.edu>
2017-02-10 23:14:33 -0800

    final project

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def new
+  	@post = Post.new
+  end
+
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def create
+  	@post = Post.new(permit_post)
+  	if @post.save
+  		flash[:success] = "Success!"
+  		redirect_to post_path(@post)
+
+  	else
+  		flash[:error] = @post.errors.full_messages
+  		rendirect_ro new_post_path
+  	end
+
+  end
+
+  private
+
+  	def permit_post
+  		params.require(:post).permit(:image, :description)
+	end
+end
